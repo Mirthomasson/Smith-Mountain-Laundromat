857852d715f6eba9f4738cf198769774bf4d5669
MirThomasson <mirthomasson@gmail.com>
2022-10-08 14:40:16 -0400

    about section

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, } from './AboutElements';
 
-const index = () => {
+const AboutSection = () => {
   return (
-    <div>
+    <>
+        <InfoContainer>
+            <InfoWrapper>
+                <InfoRow>
+                    <Column1>
+                    <TextWrapper>
+                        <TopLine>
+                            TopLine
+                        </TopLine>
+                        <Heading>Headline</Heading>
+                        <Subtitle>Subtitle</Subtitle>
+                    </TextWrapper>
+                    </Column1>
+                    <Column2>
+                    </Column2>
+                </InfoRow>
+            </InfoWrapper>
+        </InfoContainer>
       
-    </div>
+    </>
   )
 }
 
-export default index
+export default AboutSection;
